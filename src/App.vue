3cd71f1511a04e6f6007f8735d6df428c25c617d
Edimo Silva <edimossilva@gmail.com>
2022-07-18 18:48:11 -0300

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CalculatorComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorComponent from './components/CalculatorComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalculatorComponent
   }
 }
 </script>
---
+++ src/components/CalculatorComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>Calculator</h1>
+
+    {{number1}} * {{number2}} =
+
+    <input type="number" v-model="result"/>
+    <input type="button" value="responder" v-on:click.exact="calculateOnClick"/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalculatorComponent',
+  data() {
+    return {
+      number1: 1,
+      number2: 2,
+      result: 0
+    }
+  },
+  methods: {
+    calculateOnClick: function () {
+      // `this` inside methods point to the Vue instance
+      let isMultiplicationCorrect = (this.number1 * this.number2) == this.result
+      if (isMultiplicationCorrect) {
+        alert('Correct')
+        // thisrefreshNumbers()
+      } else {
+        alert('Incorrect')
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
